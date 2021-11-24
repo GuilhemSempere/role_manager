e6ce88a10cdcb1a9f75ea13a5f135deaa6a9b387
Grégori Mignerot <gregori.mignerot@cirad.fr>
2021-11-24 16:36:24 +0100

    Add the DumpMetadata object

---
+++ src/main/java/fr/cirad/security/dump/DumpMetadata.java
@@ -0,0 +1,71 @@
+package fr.cirad.security.dump;
+
+import java.util.Date;
+
+public class DumpMetadata {
+	/** Name of the dumped module */
+	private String module;
+	
+	/** Name of the dump */
+	private String name;
+	
+	/** Date of creation of the dump */
+	private Date creationDate;
+	
+	/** Description of the dump */
+	private String description;
+	
+	/** Implementation-specific identifier to the dump (typically a file path) */
+	private String identifier;
+	
+	
+	public DumpMetadata(String sIdentifier, String sModule, String sName, Date creation, String sDescription) {
+		identifier = sIdentifier;
+		module = sModule;
+		name = sName;
+		creationDate = creation;
+		description = sDescription;
+	}
+
+	public String getModule() {
+		return module;
+	}
+
+	public void setModule(String module) {
+		this.module = module;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+	
+	
+}
